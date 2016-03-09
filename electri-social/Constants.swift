fafe3cfb8b49f74e728b1df2d83e44b12bea5c10
Jim Witterschein <james.witterschein@gmail.com>
2016-03-09 05:13:41 -0500

    First commit.  Electri Social Facebook login screen layout with no coding complete.

---
+++ electri-social/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  electri-social
+//
+//  Created by Jim Witterschein on 3/8/16.
+//  Copyright © 2016 State Exam Pass. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
