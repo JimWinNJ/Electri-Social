adc33e5c37c4182ce61f25675b39b9d20dab56dc
Jim Witterschein <james.witterschein@gmail.com>
2016-03-16 12:11:01 -0400

    Built initial Post screen

---
+++ electri-social/PostCell.swift
@@ -0,0 +1,34 @@
+//
+//  PostCell.swift
+//  electri-social
+//
+//  Created by Jim Witterschein on 3/16/16.
+//  Copyright © 2016 State Exam Pass. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
