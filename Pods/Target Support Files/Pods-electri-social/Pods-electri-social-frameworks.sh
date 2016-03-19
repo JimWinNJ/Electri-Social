a5ca9e4e5e38247d1dee00b2aa7ededcad66c0a1
Jim Witterschein <james.witterschein@gmail.com>
2016-03-18 22:26:43 -0400

    Various and Sundry

--- Pods/Target Support Files/Pods-electri-social/Pods-electri-social-frameworks.sh
+++ Pods/Target Support Files/Pods-electri-social/Pods-electri-social-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-electri-social/Alamofire.framework"
   install_framework "Pods-electri-social/Firebase.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-electri-social/Alamofire.framework"
   install_framework "Pods-electri-social/Firebase.framework"
 fi
--- electri-social/DataService.swift
+++ electri-social/DataService.swift
@@ -9,13 +9,29 @@
 import Foundation
 import Firebase
 
+let _URL_BASE = "https://electri-social.firebaseio.com"
+
 class DataService {
     static let ds = DataService()
     
-    private var _REF_BASE = Firebase(url: "https://electri-social.firebaseio.com")
+    private var _REF_BASE = Firebase(url: "\(_URL_BASE)")
+    private var _REF_POSTS = Firebase(url: "\(_URL_BASE)/posts")
+    private var _REF_USERS = Firebase(url: "\(_URL_BASE)/users")
     
     var REF_BASE: Firebase {
         return _REF_BASE
+    }
+    
+    var REF_POSTS: Firebase {
+        return _REF_POSTS
+    }
+    
+    var REF_USERS: Firebase {
+        return _REF_USERS
+    }
+    
+    func createFirebaseUser(uid: String, user: Dictionary<String, String>) {
+        REF_USERS.childByAppendingPath(uid).setValue(user)
     }
     
 }
