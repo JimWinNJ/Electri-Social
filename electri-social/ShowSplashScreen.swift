5d0f05ce5c1296e137b2965d02ae2ca192b804ec
Jim Witterschein <james.witterschein@gmail.com>
2016-03-19 00:44:13 -0400

    Added custom Splash Screen with 3-second delay

---
+++ electri-social/ShowSplashScreen.swift
@@ -0,0 +1,41 @@
+//
+//  ShowSplashScreen.swift
+//  electri-social
+//
+//  Created by Jim Witterschein on 3/19/16.
+//  Copyright © 2016 State Exam Pass. All rights reserved.
+//
+
+import UIKit
+
+class ShowSplashScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        performSelector(Selector("showNavController"), withObject: nil, afterDelay: 3)
+    }
+    
+    func showNavController()
+    {
+        performSegueWithIdentifier("showSplashScreen", sender: self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
